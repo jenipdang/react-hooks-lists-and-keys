f7902a2bf6ec6c9c3a9d516bf89e152010cc97eb
Jeni Dang <jenipdang@yahoo.com>
2021-11-17 13:04:04 -0800

    commit tree f1e47025a37f368deb5b9cefc774d17f187d44e2

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ColorItem from "./ColorItem";
 
 function ColorList() {
   const colors = [
@@ -7,20 +8,36 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "purple"
   ];
+
+  /* Stactic*/
+  // return (
+  //   <div>
+  //     <h1>Top 5 CSS Colors</h1>
+  //     <ol>
+  //       <li style={{ color: colors[0] }}>{colors[0]}</li>
+  //       <li style={{ color: colors[1] }}>{colors[1]}</li>
+  //       <li style={{ color: colors[2] }}>{colors[2]}</li>
+  //       <li style={{ color: colors[3] }}>{colors[3]}</li>
+  //       <li style={{ color: colors[4] }}>{colors[4]}</li>
+  //     </ol>
+  //   </div>
+  // );
+
+/* Destructuring */
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />; 
+  });
 
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
       </ol>
     </div>
-  );
+  )
 }
 
 export default ColorList;
